6e7f928063ddfd66d75f86934601a02e6812088a
Eray-OZ <eray.py@gmail.com>
2026-05-04 14:31:32 +0300

    Initial Commit

---
+++ MacCutPaste/MacCutPasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacCutPasteApp.swift
+//  MacCutPaste
+//
+//  Created by Eray ÖZ on 4.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct MacCutPasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
